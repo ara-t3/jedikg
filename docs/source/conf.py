e1e71c45ec209782ed017f11c70f373e9243a07c
Ivan Diliso <33879857+ivandiliso@users.noreply.github.com>
2026-01-26 16:08:58 +0100

    Sitemap and Web Configuration for Search

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,15 +12,19 @@ sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath("../../"))
 
 project = 'KG-SaF: Building Complete and Curated Datasets for Machine Learning'
+
+
 copyright = '2026, Ivan Diliso and Roberto Barile'
 author = 'Ivan Diliso and Roberto Barile'
 release = '1.6.0'
+sitemap_filename = "sitemap.xml"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.mathjax',
+    'sphinx_sitemap',
     'myst_parser',  # Markdown support
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',  # For Google/NumPy docstrings
@@ -36,9 +40,13 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_baseurl = "https://ivandiliso.github.io/kg-saf/"
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
+html_title = 'KG-SaF: Official Documentation'
+html_context = {
+    "description": "Documentation for the KG-SaF-Data and KG-SaF-JDeX Workflow: Building Complete and Curated Datasets for Machine Learning"
+}
 
 source_suffix = {
     '.rst': 'restructuredtext',
