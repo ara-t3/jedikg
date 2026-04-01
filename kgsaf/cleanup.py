2d8913c3f98ef765f22e8eb26a03694b2a8ebeaa
Ivan Diliso <33879857+ivandiliso@users.noreply.github.com>
2026-04-01 12:51:17 +0200

    ApuliaTravel Fix and ATRAVEL New Dataset

--- kgsaf/cleanup.py
+++ kgsaf/cleanup.py
@@ -1,17 +1,18 @@
 from kgsaf.jdex import JDEX
 from pathlib import Path
+from kgsaf.tools.reasoner import PresetAxioms
 
 if __name__ == "__main__":
     jdex = JDEX.from_json("./test/test_data/config.json")
     reasoner = jdex.reasoner
 
     if True:
-        satif = reasoner.satisfiability(Path("/home/navis/Devel/PhD/kg-saf/kgsaf/data/datasets/unpack/WHOW_5_ROFF/ontology.owl"), verbose=2)
+        satif = reasoner.satisfiability(Path("/home/navis/Devel/PhD/kg-saf/kgsaf/data/datasets/unpack/ATRAVEL_ROFF/ontology.owl"), verbose=2)
 
     if False:
 
-        base = Path("/home/navis/Devel/PhD/kg-saf/kgsaf/data/datasets/old_unpack/WHOW25-5-BASE/abox")
-        out_base = Path("/home/navis/Devel/PhD/kg-saf/kgsaf/data/triples/whow5.owl")
+        base = Path("/home/navis/Devel/PhD/kg-saf/kgsaf/data/datasets/old_unpack/ATRAVEL-BASE/abox")
+        out_base = Path("/home/navis/Devel/PhD/kg-saf/kgsaf/data/facts/unpack/atravel.owl")
         
 
         reasoner.merging([
--- kgsaf/main.py
+++ kgsaf/main.py
@@ -2,6 +2,6 @@ from kgsaf.jdex import JDEX
 
 
 if __name__ == "__main__":
-    jdex = JDEX.from_json("./kgsaf/data/configurations/WHOW_5_RON.json")
+    jdex = JDEX.from_json("./kgsaf/data/configurations/ATRAVEL_RON.json")
     jdex.run()
 
