2713e8edb84c4c3ec6c0f05de4f1df7dc5566304
Ivan Diliso <33879857+ivandiliso@users.noreply.github.com>
2026-04-10 16:59:53 +0200

    ERA Initial Analysis

--- jdex/test.py
+++ jdex/test.py
@@ -9,9 +9,27 @@ from rdflib import URIRef
 
 if __name__ == "__main__":
     """"""
-    jdex = JDEX.from_json(Path("/Users/navis/dev/projects/kg-saf/data/configurations/TEST.json"))
-    jdex.run()
-    
+    #jdex = JDEX.from_json(Path("/Users/navis/dev/projects/kg-saf/data/configurations/TEST.json"))
+    #jdex.run()
+
+    g = Graph()
+    g.parse("/home/navis/dev/kg-saf-workspace/kg-saf/data/schemas/unpack/ERA/ontology.owl")
+
+    obj_props = set(g.subjects(RDF.type, OWL.ObjectProperty))
+    data_props = set(g.subjects(RDF.type, OWL.DatatypeProperty))
+
+    print(obj_props & data_props)
+
+    with open("/home/navis/dev/kg-saf-workspace/kg-saf/data.txt", "r") as f:
+        d = f.readlines()
+        vals = {URIRef(a.strip("<>").strip("\n").strip(">")) for a in d}
+
+    print(len(obj_props & vals))
+    print(len(obj_props - data_props))
+
+    tot = 33628772
+
+                
     
 
 
